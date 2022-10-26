993de95e8b4b5414838ed113bbbbf221236a5817
ryan-lupps <113069697+ryan-lupps@users.noreply.github.com>
2022-10-26 13:46:58 -0400

    Update RatingClass.py

--- MoviePackage/RatingClass.py
+++ MoviePackage/RatingClass.py
@@ -1,5 +1,32 @@
 '''
-Created on Oct 23, 2022
-
-@author: sethapple
+#Name: Seth Medlin, Addyson Stansel, Ryan Lupton, Tyra __
+#email: luptonrj@mail.uc.edu
+#Assignment: Assignment 09
+#Course: IS 4010
+#Semester/Year: Fall 2022
+#Brief Description: This project demonstrates that we can work together in a group to produce modular code
+#Citations: 
+#Anything else thats relevant: N/A
 '''
+class Rating():
+    def setRating(self, rating):
+        self.validateRating(rating)
+    def validateRating(self, rating):
+        if rating < 0:
+            print ("A movie cannot be rated below 0")
+        else:
+            self.rating = rating
+        if rating > 100:
+            print ("A movie cannot be rated above 100")
+        else:
+            self.rating = rating 
+             
+    def __init__(self, movieTitle, rating):
+        self.movieTitle = movieTitle  
+        self.validateRating(rating)
+       
+    def __repr__(self):
+        return "Score: " + self.movieTitle + self.rating
+    
+    def __str__(self):
+        return "Score: " + self.movieTitle + str(self.rating) 
